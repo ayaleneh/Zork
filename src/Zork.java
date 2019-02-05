ce208dc4cbbcb7665f740b4feac245facb1b3b31
ayaleneh <getaunayaleneh@gmail.com>
2019-02-05 16:56:23 -0500

    commit 2

--- src/Zork.java
+++ src/Zork.java
@@ -1,93 +1,142 @@
+package com.company;
 import java.util.Scanner;
 
-public class Zork {
+class Zork {
+    public static int counter=0;
     public static void main(String[] args) {
-
         foyer();
-
     }
     public static void foyer(){
-
-        System.out.println("door to  room 2");
+        counter++;
+        System.out.println("ROOM 1 AND CONTAIN  IT CONTAIN DEAD SCORPION");
+        System.out.println();
+        System.out.println("door to  ROOM 2");
         front_room();
     }
     public static void front_room(){
+        counter++;
         String answer="";
-        System.out.println("ROOM 2 AND CONTAIN PIANO");
+        System.out.println("ROOM 2 AND IT CONTAIN PIANO");
         System.out.println();
         System.out.println("you have currently three option to d/t room");
-        System.out.println("which room you want to travel..(s1|w3|e4)");
-        Scanner scanner=new Scanner(System.in);
-        answer=scanner.nextLine();
-        switch(answer){
-            case "s1":
-                foyer();
-                break;
-            case "w3":
-                library();
-                break;
-            case "e4":
-                kitchen();
-                break;
-                default:
-                    System.out.println("please enter the correct direction");
+        while(true){
+            System.out.println("which room you want to travel..(s1|w3|e4) OR press 'q' to exit");
+            Scanner scanner=new Scanner(System.in);
+            answer=scanner.nextLine();
+            if(!(answer.equalsIgnoreCase("q"))){
+                switch(answer){
+                    case "s1":
+                        foyer();
+                        break;
+                    case "w3":
+                        library();
+                        break;
+                    case "e4":
+                        kitchen();
+                        break;
+                    default:
+                        System.out.println("please enter the correct direction");
+                }
+            }
+            else if((answer.equalsIgnoreCase("q"))){
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
+            }
+            else {
+                System.out.println("please enter the correct choice");
+            }
+
         }
+
 
     }
     public static void library(){
+        counter++;
         String answer="";
         System.out.println("ROOM 3 AND CONTAIN SPIDERS");
         System.out.println();
         System.out.println("you have currently two option to d/t room");
-        System.out.println("which room you want to travel..(e2|n5)");
-        Scanner scanner=new Scanner(System.in);
-        answer=scanner.nextLine();
-        switch (answer){
-            case "e2":
-                front_room();
-                break;
-            case "n5":
-                dining_room();
-                break;
-                default:
-                    System.out.println("please enter the correct direction");
+        while(true){
+            System.out.println("which room you want to travel..(e2|n5)  OR press 'q' to exit");
+            Scanner scanner=new Scanner(System.in);
+            answer=scanner.nextLine();
+            if(!(answer.equalsIgnoreCase("q"))){
+                switch (answer){
+                    case "e2":
+                        front_room();
+                        break;
+                    case "n5":
+                        dining_room();
+                        break;
+                    default:
+                        System.out.println("please enter the correct direction");
+                }
+            }
+            else if((answer.equalsIgnoreCase("q"))){
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
+            }
+            else {
+                System.out.println("please enter the correct choice");
+            }
+
         }
+
     }
     public static void kitchen(){
+        counter++;
         String answer="";
         System.out.println("ROOM 4 AND CONTAIN KITCHEN");
         System.out.println();
         System.out.println("you have currently two option to d/t room");
-        System.out.println("which room you want to travel..(w2|n7)");
-        Scanner scanner=new Scanner(System.in);
-        answer=scanner.nextLine();
-        switch(answer){
-            case "w2":
-                front_room();
-                break;
-            case "n7":
-                parlor();
-                break;
-                default:
-                    System.out.println("please enter the correct room direction");
+        while(true){
+            System.out.println("which room you want to travel..(w2|n7)  OR press 'q' to exit");
+            Scanner scanner=new Scanner(System.in);
+            answer=scanner.nextLine();
+            if(!(answer.equalsIgnoreCase("q"))){
+                switch(answer){
+                    case "w2":
+                        front_room();
+                        break;
+                    case "n7":
+                        parlor();
+                        break;
+                    default:
+                        System.out.println("please enter the correct room direction");
+                }
+            }
+            else if((answer.equalsIgnoreCase("q"))){
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
+            }
+            else {
+                System.out.println("please enter the correct choice");
+            }
+
         }
+
     }
     public static void dining_room(){
+        counter++;
         String answer="";
         System.out.println("ROOM 5 AND CONTAIN DUST EMPTY BOX");
         System.out.println();
         System.out.println("you have currently ONE option to d/t room");
 
         while (true){
-            System.out.println("which room you want to travel..(s3)");
-            System.out.println("press 'q' to RETURN TO PREVIOUS ROOM");
+            System.out.println("which room you want to travel..(s3)  OR press 'q' to exit");
             Scanner scanner=new Scanner(System.in);
             answer=scanner.nextLine();
             if(answer.equalsIgnoreCase("s3")){
                 library();
             }
             else if(answer.equalsIgnoreCase("q")){
-                library();
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
             }
             else {
                 System.out.println("please enter the correct choice");
@@ -95,13 +144,13 @@ public class Zork {
         }
     }
     public static void valut(){
+        counter++;
         String answer="";
         System.out.println("ROOM 6 AND CONTAIN 3 WALKING SKELETONS");
         System.out.println();
         System.out.println("you have currently TWO option to d/t room");
         while(true){
-            System.out.println("which room you want to travel..(e7|e8)");
-            System.out.println("press 'q' to RETURN TO PREVIOUS ROOM");
+            System.out.println("which room you want to travel..(e7|e8)  OR press 'q' to exit");
             Scanner scanner=new Scanner(System.in);
             answer=scanner.nextLine();
             int random=(int)(Math.random()*10+1);
@@ -113,7 +162,9 @@ public class Zork {
                 }
             }
             else if(answer.equalsIgnoreCase("q")){
-                System.out.println("here is the previous ROOM");
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
             }
             else{
                 System.out.println("please enter the correct choice");
@@ -121,23 +172,25 @@ public class Zork {
         }
     }
     public static void parlor(){
+        counter++;
         String answer="";
         System.out.println("ROOM 7 AND CONTAIN TREASURE CHEST");
         System.out.println();
         System.out.println("you have currently TWO option to d/t room");
         while (true){
-            System.out.println("which room you want to travel..(e6|s4)");
-            System.out.println("press 'q' to RETURN TO PREVIOUS ROOM");
+            System.out.println("which room you want to travel..(e6|s4)  OR press 'q' to exit");
             Scanner scanner=new Scanner(System.in);
             answer=scanner.nextLine();
             if(answer.equalsIgnoreCase("e6")){
-                library();
+                valut();
             }
-            else if(){
-
+            else if(answer.equalsIgnoreCase("s4")){
+                kitchen();
             }
             else if(answer.equalsIgnoreCase("q")){
-                library();
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
             }
             else {
                 System.out.println("please enter the correct choice");
@@ -145,7 +198,29 @@ public class Zork {
         }
     }
     public static void secret_room(){
+        counter++;
+        String answer="";
+        System.out.println("YOU ARE IN SECRET ROOM RIGHT NOW");
+        System.out.println("ROOM 8 AND CONTAIN PILES OF GOLD");
+        System.out.println();
+        System.out.println("you have currently ONE option to d/t room");
+        while (true){
+            System.out.println("which room you want to travel..(w6)  OR press 'q' to exit");
 
+            Scanner scanner=new Scanner(System.in);
+            answer=scanner.nextLine();
+            if(answer.equalsIgnoreCase("w6")){
+                valut();
+            }
+            else if(answer.equalsIgnoreCase("q")){
+                System.out.println("YOU VISIT "+counter +" ROOM");
+                System.out.println("THANKS");
+                System.exit(0);
+            }
+            else {
+                System.out.println("please enter the correct choice");
+            }
+        }
     }
 
 }
